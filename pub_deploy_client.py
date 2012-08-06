b9fa0cb7bc3409aec5bc2b9dfe039e69c14f57dd
thinkfurther <kkxxywn@gmail.com>
2012-08-06 18:17:41 +0800

    Rewrite and add some functions

--- pub_deploy_client.py
+++ pub_deploy_client.py
@@ -1,95 +1,82 @@
-import paramiko,os,pexpect,zipfile,getpass
-homefolder=os.path.expanduser("~/")
+import paramiko,os,getpass,time
 
 def addpub(local,server):
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(server['host'], int(server['port']),server['username'], server['password'])
-        ssh.exec_command('unzip ~/pubkey.zip -d ~/pubkey/')
-        ssh.exec_command('python ~/pubkey/pub_deploy_server.py')
-        #ssh.exec_command('python ~/pubkey/pub_deploy_server.py')
-        ssh.exec_command('rm -f ~/pubkey.zip')        
-        ssh.exec_command('rm -rf ~/pubkey/')
-        os.system('rm -f ~/pubkey.zip')
+        ssh.exec_command('python ~/pub_deploy_server.py')
+        time.sleep(0.1)
+        ssh.exec_command('rm -f ~/pubkey.pub')
+        ssh.exec_command('rm -f ~/pub_deploy_server.py')
         ssh.close()
-        print 'Your public key has been added to server authorized_keys'
+        print 'Your public key has been added to senrver authorized_keys'
     except paramiko.SSHException, e:
         print e
 
 def uploadpub(local,server):
-    srv=zipfile.ZipFile(os.path.expanduser('~/pubkey.zip'),'w')
+    transport=paramiko.Transport((server['host'],int(server['port'])))
+    transport.connect(username=server['username'],password=server['password'])
+    sftp=paramiko.SFTPClient.from_transport(transport)
+    remotefile1=''.join(['/home/',server['username'],'/','pubkey.pub'])
+    localfile1=local['publickey']
+    sftp.put(localfile1,remotefile1)
+    remotefile2=''.join(['/home/',server['username'],'/','pub_deploy_server.py'])
     temp=__file__.split('/')
     temp[-1]='pub_deploy_server.py'
     serverfile='/'.join(temp)
-    srv.write(serverfile,'pub_deploy_server.py')
-    srv.write(local['publickey'],'publickey.pub')
-    srv.extractall(os.path.expanduser('~/1'))
-    srv.close()
-    cmd="scp -P %s %s %s@%s:~/" %(server['port'],local['compressfile'],server['username'],server['host'])
-    try:
-        scp=pexpect.spawn(cmd)  
-        i=scp.expect(['password:', 'continue connecting (yes/no)?'],timeout=5)
-        if i == 0:
-            scp.sendline(server['password'])
-        elif i == 1:
-            scp.sendline("yes")
-            scp.expect(['password:'])  
-            scp.sendline(server['password']) 
-        scp.expect(pexpect.EOF)
-        scp.close()
-        print "Trandfer has done"
-    except pexpect.TIMEOUT:  
-        print 'Connection timeout'  
-    except pexpect.EOF:
-        print 'Trandfer connection exit'  
-        scp.close()  
-    except Exception,e:  
-        print "Connection close",e
-        scp.close()
-        
+    localfile2=serverfile
+    sftp.put(localfile2,remotefile2)
+    sftp.close() 
+    transport.close()
+    print "Trandfer has done"
+
+def infoinput(local,server):
+    while True:
+        server['host']=raw_input('please input server ip:')
+        if not server['host']:
+            print 'You do not input ip,please reinput'
+            continue     
+        else:
+            break
+    while True:
+        server['port']=raw_input('please input server ssh port(default for 22):')
+        if not server['port']:
+            server['port']='22'
+        break
+    while True:
+        server['username']=raw_input('please input login username:')
+        if not server['username']:
+            print 'You do not input username,please reinput'
+            continue     
+        else:
+            break        
+    while True:
+        server['password']=getpass.getpass('please input login password:')
+        if not server['password']:
+            print 'You do not input password,please reinput'
+            continue     
+        else:
+            break     
+    while True:
+        if local['publickey']:
+            break
+        else:
+            local['publickey']=raw_input('please input where your public key is(absolute path):')
+            if not local['publickey']:
+                print 'You do not input you public file,please reinput'
+                continue
+            else:
+                break    
+    return (local,server)
+    
 def main():
     local={'known_host':os.path.expanduser("~/.ssh/known_hosts"),
            'compressfile':os.path.expanduser('~/pubkey.zip'),
            'publickey':''}
-    server={'host':"115.156.219.152",'port':'22','username':"root",'password':"AP@ssw0rd"}
+    server={'host':"",'port':'','username':"",'password':""}
     while True:
-        while True:
-            server['host']=raw_input('please input server ip:')
-            if not server['host']:
-                print 'You do not input ip,please reinput'
-                continue     
-            else:
-                break
-        while True:
-            server['port']=raw_input('please input server ssh port(default for 22):')
-            if not server['port']:
-                server['port']='22'
-            break
-        while True:
-            server['username']=raw_input('please input login username:')
-            if not server['username']:
-                print 'You do not input username,please reinput'
-                continue     
-            else:
-                break        
-        while True:
-            server['password']=getpass.getpass('please input login password:')
-            if not server['password']:
-                print 'You do not input password,please reinput'
-                continue     
-            else:
-                break     
-        while True:
-            if local['publickey']:
-                break
-            else:
-                local['publickey']=raw_input('please input where your public key is(absolute path):')
-                if not local['publickey']:
-                    print 'You do not input you public file,please reinput'
-                    continue
-                else:
-                    break
+        local,server=infoinput(local,server)
         uploadpub(local,server)
         addpub(local,server)
         goon=raw_input("Do you have other server to deploy public key('y'or 'n'):")
--- pub_deploy_server.py
+++ pub_deploy_server.py
@@ -1,11 +1,8 @@
-import os,sys
-files=os.listdir(os.path.expanduser('~/pubkey/'))
-currentfile=sys.argv[0][sys.argv[0].rfind(os.sep)+1:]
-files.remove(currentfile)
+import os
 if not os.path.exists(os.path.expanduser('~/.ssh/')):
     os.system('mkdir ~/.ssh')
 if os.path.isfile(os.path.expanduser('~/.ssh/authorized_keys')):
-    clientkeyfile=open(os.path.expanduser('~/pubkey/%s'%files[0]))
+    clientkeyfile=open(os.path.expanduser('~/pubkey.pub'))
     clientkey=clientkeyfile.read()
     authkeyfile=open(os.path.expanduser('~/.ssh/authorized_keys'))
     found=0
@@ -16,9 +13,9 @@ if os.path.isfile(os.path.expanduser('~/.ssh/authorized_keys')):
         else:
             found=0
     if found==0:
-        os.system('cat ~/pubkey/%s >> ~/.ssh/authorized_keys' %files[0])
+        os.system('cat ~/pubkey.pub >> ~/.ssh/authorized_keys')
 else:
-    os.system('cat ~/pubkey/%s > ~/.ssh/authorized_keys' %files[0])
+    os.system('cat ~/pubkey.pub > ~/.ssh/authorized_keys')
 
 os.system('chmod 0700 ~/.ssh')
 os.system('chmod 0600 ~/.ssh/authorized_keys')    
